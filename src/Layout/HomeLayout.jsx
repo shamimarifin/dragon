75c5001c14ead191d0c1d0452f5d657c096b7790
shamimarifin <shamimarifin790@gmail.com>
2025-12-28 14:35:58 +0600

    Sticky behaviour

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -20,7 +20,7 @@ const HomeLayout = () => {
             </nav>
         </header>
         <main className='w-11/12 mx-auto my-3 grid grid-cols-12 gap-5'>
-            <aside className='col-span-2'>
+            <aside className='col-span-2 sticky top-0 h-fit'>
                 <LeftAside></LeftAside>
             </aside>
             <section className="main col-span-8">
