3464686c5d1f76ab11554e32b25da5bdd53ed8fc
shamimarifin <shamimarifin790@gmail.com>
2025-12-30 18:09:44 +0600

    Register User & OnAuthStateChange

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,65 +1,110 @@
-import React, { use, useState } from 'react'
-import { Link } from 'react-router'
-import { AuthContext } from '../../Provider/AuthProvider'
-
+import React, { use, useState } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Register = () => {
-  
-  const {createUser,setUser} = use(AuthContext)
-  const [error , setError] = useState()
+  const { createUser, setUser } = use(AuthContext);
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState(false);
 
 
-  // Reset Status
-
-  const handleRegister = (e)=> {
-    e.preventDefault()
+  const handleRegister = (e) => {
+    e.preventDefault();
     const form = e.target;
     const name = form.name.value;
     const photo = form.photo.value;
     const email = form.email.value;
     const password = form.password.value;
 
+    console.log(name, photo, email, password);
+
+    // Reset Status
+    setError("")
+    setSuccess("")
 
     createUser(email, password)
-    .then((result)=> {
-      const user = result.user
-      setUser(user)
-    }).catch((error)=> {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      setError(errorMessage)
-    })
-  }
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
         <div className="card-title flex justify-center">
-            <h2 className="font-semibold text-center text-2xl">Register your account</h2>
+          <h2 className="font-semibold text-center text-2xl">
+            Register your account
+          </h2>
         </div>
         <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
-            <label className='label'>Your Name</label>
-            <input type="text" className="input" name="name" placeholder="Enter your name" required/>
-            <label className='label'>Photo Url</label>
-            <input type="text" className="input" name="photo" placeholder="Enter your url" required/>
+            <label className="label">Your Name</label>
+            <input
+              type="text"
+              className="input"
+              name="name"
+              placeholder="Enter your name"
+              required
+            />
+            <label className="label">Photo Url</label>
+            <input
+              type="text"
+              className="input"
+              name="photo"
+              placeholder="Enter your url"
+              required
+            />
             <label className="label">Email</label>
-            <input type="email" className="input" name="email" placeholder="Email" required/>
+            <input
+              type="email"
+              className="input"
+              name="email"
+              placeholder="Email"
+              required
+            />
             <label className="label">Password</label>
-            <input type="password" className="input" name="password" placeholder="Password" required/>
-            
-            <button type='submit' className="btn btn-neutral mt-4">Register</button>
+            <input
+              type="password"
+              className="input"
+              name="password"
+              placeholder="Password"
+              required
+            />
 
-            <p className="mt-5 text-center font-semibold">Already Have An Account ? <Link to="/auth/login" className="text-secondary">Login</Link></p>
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
+
+            <p className="mt-5 text-center font-semibold">
+              Already Have An Account ?{" "}
+              <Link to="/auth/login" className="text-secondary">
+                Login
+              </Link>
+            </p>
           </fieldset>
 
-          <div className='mt-4'>
-            <p className='text-red-400'>{error}</p>
+          <div className="mt-4">
+            {
+              error && (
+                <p className="text-red-400"> ইরর খাইছস বোকাচুদা </p>
+              )
+            }
+
+            {success && (
+              <p className="text-green-400">Account create successfully</p>
+            )}
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
